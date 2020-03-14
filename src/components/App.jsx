f437e69829c20c22006d853c2b339f05b3a6d9a4
jgam <jgam@alumni.nd.edu>
2020-03-14 12:01:39 +0900

    component practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import LifeCycleSample from './practice/LifeCycleSample';
 
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <div>
+      hello world!
+      <div>
+        Lifecycle sample!!
+        <LifeCycleSample />
+      </div>
+    </div>
+  );
 }
 
 export default App;
